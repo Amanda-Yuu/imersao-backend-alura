5d2084b663b251a2b794e18b9c444b5976a74747
amanda silveira <amanda.silveira0w@gmail.com>
2024-11-24 18:27:36 -0300

    aula 2

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import express from "express";
 
-const app = express()
+    const posts = [
+        { id: 1, descricao: 'Uma foto teste', imagem: 'https://placecats.com/millie/300/150' },
+        { id: 2, descricao: 'Gato brincando com um novelo de lã', imagem: 'https://placecats.com/felix/200/300' },
+        { id: 3, descricao: 'Gato fazendo panqueca', imagem: 'https://placecats.com/felix/200/300' },
+ ];
+
+const app = express();
+app.use(express.json())
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vindas à imersão!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPostsPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostsPorID(req.params.id)
+    res.status(200).json(posts[index]);
+
 });
